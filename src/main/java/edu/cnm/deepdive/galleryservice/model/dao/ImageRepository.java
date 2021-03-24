fca136d15304713f7620ab572d1e3355b477b923
darbach <danielarbach@gmail.com>
2021-03-24 12:00:40 -0600

    Added list of images.

--- src/main/java/edu/cnm/deepdive/galleryservice/model/dao/ImageRepository.java
+++ src/main/java/edu/cnm/deepdive/galleryservice/model/dao/ImageRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ImageRepository extends JpaRepository<Image, UUID> {
 
-  // TODO Add search methods.
+  Iterable<Image> getAllByOrderByCreatedDesc();
+
 }
